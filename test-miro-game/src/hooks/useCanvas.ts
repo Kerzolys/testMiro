6c188e7ae27b93c7f44e2f044b4ad167c80b9654
kerzolys <kerzolys@gmail.com>
2024-09-02 17:22:45 +0300

    исправил отображение размеров канваса после продакшена - ввел dpr

--- test-miro-game/src/hooks/useCanvas.ts
+++ test-miro-game/src/hooks/useCanvas.ts
@@ -1,24 +1,39 @@
 import { useEffect, useRef } from "react";
 
 export const useCanvas = (
-  draw: (context: CanvasRenderingContext2D,ratio: number) => void,
-  
+  draw: (context: CanvasRenderingContext2D, ratio: number) => void
 ) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
+
     const baseHeight = 720;
     let animationId: number;
     if (canvas) {
-      const ratio = canvas.height / baseHeight;
-      canvas.width = window.innerWidth - (10 * ratio);
-      canvas.height = window.innerHeight - (200 * ratio);
+      // const ratio = canvas.height / baseHeight;
+      const dpr = window.devicePixelRatio || 1;
+
+      canvas.width = (window.innerWidth - 10) * dpr;
+      canvas.height = (window.innerHeight - 200) * dpr;
+
+      canvas.style.width = canvas.width + "px";
+      canvas.style.height = canvas.height + "px";
+
       const context = canvas.getContext("2d");
-      console.log(window)
+      console.log(
+        `Canvas width: ${canvas.width}, Canvas height: ${canvas.height}`
+      );
+      console.log(
+        `Window innerWidth: ${window.innerWidth}, innerHeight: ${window.innerHeight}`
+      );
+      console.log(`Device Pixel Ratio: ${window.devicePixelRatio}`);
       if (context) {
+        context.scale(dpr, dpr);
+        const ratio = canvas.height / (baseHeight * dpr);
+
         const render = () => {
-          draw(context,ratio);
+          draw(context, ratio);
           animationId = window.requestAnimationFrame(render);
         };
         render();
